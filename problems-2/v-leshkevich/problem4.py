73224172b014381dcf89db3d8cfbe830300cec86
Jlemix <unuashechka@gmail.com>
2021-03-02 12:36:49 +0700

    upd.

--- problems-2/v-leshkevich/problem4.py
+++ problems-2/v-leshkevich/problem4.py
@@ -7,10 +7,11 @@ try:
 
     with open(path, 'r') as file:
         text = ''.join(file.read().split())
-        result = str.find(text, inp)
-        while result != -1:
-            occurrences.append(result - 2)
-            result = str.find(text, inp, result + 1)
+
+    result = str.find(text, inp)
+    while result != -1:
+        occurrences.append(result - 2)
+        result = str.find(text, inp, result + 1)
 
     print('Found {n} results.\nPositions: {p} ...'.format(n=len(occurrences),
                                                           p=' '.join([str(value) for value in occurrences[:5]])))
