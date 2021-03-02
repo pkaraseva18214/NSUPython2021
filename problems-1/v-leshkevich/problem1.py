adb6248840aec17d856ded34d6dbc999f7ebb249
pkaraseva18214 <karasyova_polina@mail.ru>
2021-03-02 09:47:34 +0700

    added tests

--- problems-1/v-leshkevich/problem1.py
+++
@@ -1,9 +0,0 @@
-def cusum(_list):
-    total_sum = 0
-    result = [0]
-    for num in _list:
-        total_sum += num
-        result.append(total_sum)
-    return result
-
-print(cusum(list(map(int, input().split()))))
