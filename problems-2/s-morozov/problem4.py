623976d16f9756fe2a53dfc7074ab646195c85c9
Simon Morozov <morozzmorozov@gmail.com>
2021-03-12 00:03:01 +1100

    add problem 2-4

---
+++ problems-2/s-morozov/problem4.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+seq = input('Enter sequence to search for.\n')
+
+with open('pi.txt', 'r') as file:
+  pi = file.read().replace('\n', '')
+
+pos_cnt = 0
+positions = []
+
+start_pos = 2
+cur_pos = pi.find(seq, start_pos)
+
+while cur_pos != -1:
+  pos_cnt += 1
+  if (pos_cnt <= 5):
+    positions.append(cur_pos - start_pos)
+  cur_pos = pi.find(seq, cur_pos + 1)
+
+print(f'Found {pos_cnt} results.\nPositions: {" ".join(map(str, positions))} ...')
+
