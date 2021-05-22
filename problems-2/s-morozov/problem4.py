08240c2e48d6c76fcecd9b03335104f03a1f9082
Simon Morozov <morozzmorozov@gmail.com>
2021-05-22 18:35:08 +1100

    Add test

--- problems-2/s-morozov/problem4.py
+++ problems-2/s-morozov/problem4.py
@@ -2,26 +2,29 @@
 
 import sys
 
-if len(sys.argv) == 1:
-  seq = input('Enter sequence to search for.\n')
-else:
-  seq = sys.argv[1]
+def find_in_pi(seq, pi):
+  pos_cnt = 0
+  positions = []
+  start_pos = pi.find('.', 0) + 1
+  cur_pos = pi.find(seq, start_pos)
 
+  while cur_pos != -1:
+    pos_cnt += 1
+    if (pos_cnt <= 5):
+      positions.append(cur_pos - start_pos)
+    cur_pos = pi.find(seq, cur_pos + 1)
 
-with open('pi.txt', 'r') as file:
-  pi = file.read().replace('\n', '')
+  return pos_cnt, positions
 
-pos_cnt = 0
-positions = []
-
-start_pos = 2
-cur_pos = pi.find(seq, start_pos)
-
-while cur_pos != -1:
-  pos_cnt += 1
-  if (pos_cnt <= 5):
-    positions.append(cur_pos - start_pos)
-  cur_pos = pi.find(seq, cur_pos + 1)
-
-print(f'Found {pos_cnt} results.\nPositions: {" ".join(map(str, positions))} ...')
-
+if __name__ == '__main__':
+  if len(sys.argv) == 1:
+    seq = input('Enter sequence to search for.\n')
+  else:
+    seq = sys.argv[1]
+  try:
+    with open('pi.txt', 'r') as file:
+      pi = file.read().replace('\n', '')
+    pos_cnt, positions = find_in_pi(seq, pi)
+    print(f'Found {pos_cnt} results.\nPositions: {" ".join(map(str, positions))} ...')
+  except IOError as e:
+    print(f'Unable to open file: {str(e)}', file = sys.stderr)
