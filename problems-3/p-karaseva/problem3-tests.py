3a6585e9195045ac75249fa997582a29d713523e
pkaraseva18214 <karasyova_polina@mail.ru>
2021-05-24 23:49:44 +0700

    problem 3 3

---
+++ problems-3/p-karaseva/problem3-tests.py
@@ -0,0 +1,44 @@
+import unittest
+from problem3 import Vector
+
+class MyTestCase(unittest.TestCase):
+    def test_1(self):
+        v1 = Vector(1, 2, 3)
+        self.assertEqual(v1.__str__(), '(1, 2, 3)')
+
+    def test_2(self):
+        v1 = Vector(4, 3)
+        self.assertEqual(v1.length(), 5)
+
+    def test_3(self):
+        v1 = Vector(4, 3, 10, 11, 54, 1, 20)
+        self.assertEqual(v1.get_element(4), 54)
+
+    def test_4(self):
+        v1 = Vector(4, 3, 10, 11, 54, 1, 20)
+        self.assertEqual(v1.multiply_by_const(10), [40, 30, 100, 110, 540, 10, 200])
+
+    def test_5(self):
+        v1 = Vector(4, 3, 10, 11, 54, 1, 20)
+        v2 = Vector(4, 3, 2, 1, 0, 4, 70)
+        self.assertEqual(v1.equals(v2), False)
+
+    def test_6(self):
+        v1 = Vector(4, 3, 10, 11, 54, 1, 20)
+        v2 = Vector(4, 3, 2, 1, 0, 4, 70)
+        self.assertEqual(v1.sum(v2), [8, 6, 12, 12, 54, 5, 90])
+
+    def test_7(self):
+        v1 = Vector(4, 3, 10, 11, 54, 1, 20)
+        v2 = Vector(4, 3, 2, 1, 0, 4, 70)
+        self.assertEqual(v1.subtract(v2), [0, 0, 8, 10, 54, -3, -50])
+
+    def test_8(self):
+        v1 = Vector(9, 4, 7, 8)
+        v2 = Vector(5, 10, 1, 6)
+        self.assertEqual(v1.scalar_product(v2), 140)
+
+if __name__ == '__main__':
+    unittest.main()
+
+
---
+++ problems-3/p-karaseva/problem3.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+import itertools
+import math
+
+class Vector:
+
+    def __init__(self, *args):
+        self.values = []
+        self.count = len(args)
+        for x in args:
+            self.values.append(x)
+
+    def __str__(self):
+        s = '('
+        count = 0
+        for x in self.values:
+            count += 1
+            if count < self.count:
+                s += str(x) + ', '
+            else:
+                s += str(x) + ')'
+        return s
+
+    def length(self):
+        l = 0
+        for x in self.values:
+            l += x ** 2
+        return math.sqrt(l)
+
+    def multiply_by_const(self, num):
+        added = list()
+        for n in self.values:
+            added.append(n * num)
+        self.values = added
+        return self.values
+
+    def get_element(self, index):
+        return self.values[index]
+
+    def equals(self, v2):
+        if self.values == v2.values:
+            return True
+        return False
+
+    def sum(self, v2):
+        k = 0
+        while k < self.count:
+            self.values[k] += v2.values[k]
+            k += 1
+        return self.values
+
+    def subtract(self, v2):
+        k = 0
+        while k < self.count:
+            self.values[k] -= v2.values[k]
+            k += 1
+        return self.values
+
+    def scalar_product(self, v2):
+        k = 0
+        res = 0
+        while k < self.count:
+            res += self.values[k] * v2.values[k]
+            k += 1
+        return res
+
